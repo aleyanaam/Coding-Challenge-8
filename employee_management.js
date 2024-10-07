3afc6a198a3ace34b0e42d90a1f13572c03ee3f2
Aleyana McLeod <aleyanam@usf.edu>
2024-10-06 23:24:29 -0400

    Create Employee class with properties and methods

--- employee_management.js
+++ employee_management.js
@@ -1,1 +1,15 @@
 //Task 1
+//Creation of class with relevant employee information
+class Employee {
+    constructor(name, salary, position, department) {
+      this.name = name;
+      this.salary = salary;
+      this.position = position;
+      this.department = department;
+      
+    }
+  
+    getDetails() {
+      console.log(`${this.name} works as a ${this.position} earning $${this.salary} per year.`);
+    }
+  }
